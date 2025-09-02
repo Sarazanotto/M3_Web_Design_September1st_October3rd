bb7857dd304918ae59b993fee6ed484aaf27529c
sara zanotto <saraazanotto@gmail.com>
2025-09-02 18:36:46 +0200

    D1

---
+++ W1/W1D1/EXERCISE_HTML_CSS/main.js
@@ -0,0 +1,8 @@
+/*EXTA FACOLTATIVI:
+
+Crea con Js la funzionalità per rimuovere il link "twitter" sotto alla sezione "Altro" nell'elemento "aside". Deve avvenire al caricamento della pagina, automaticamente.
+
+Crea con Js la funzionalità per rimuovere il corrispondente elemento padre dal DOM cliccando sul link "Continua a leggere".
+
+Crea con Js la funzionalità per creare un alert col nome dell'autore ogni volta che il cursore passa sopra l'autore del post.*/
+
