81f33bbcb89d7c92a9ae53defe88280966c8d422
sara zanotto <saraazanotto@gmail.com>
2025-09-03 11:07:56 +0200

    esercizi extra js

--- W1/W1D1/EXERCISE_HTML_CSS/main.js
+++ W1/W1D1/EXERCISE_HTML_CSS/main.js
@@ -1,8 +1,32 @@
-/*EXTA FACOLTATIVI:
+// EXTRA Crea con JavaScript la funzionalità per rimuovere il link "twitter" sotto alla sezione "Altro" nell'elemento "aside". 
+// Deve avvenire al caricamento della pagina, automticamente.
 
-Crea con Js la funzionalità per rimuovere il link "twitter" sotto alla sezione "Altro" nell'elemento "aside". Deve avvenire al caricamento della pagina, automaticamente.
+const linkTwitter= document.querySelector(".blog-sidebar .p-4:nth-of-type(3) ol li:nth-of-type(2)")
 
-Crea con Js la funzionalità per rimuovere il corrispondente elemento padre dal DOM cliccando sul link "Continua a leggere".
+function linkTwitterRemouve(){
+    if(linkTwitter)
+        linkTwitter.remove()
+}
+linkTwitterRemouve()
 
-Crea con Js la funzionalità per creare un alert col nome dell'autore ogni volta che il cursore passa sopra l'autore del post.*/
+// EXTRA Crea con JavaScript la funzionalità per rimuovere il corrispondente elemento padre dal DOM cliccando sul link "Continua a leggere".
 
+
+
+
+
+// EXTRA Crea con JavaScript la funzionalità per creare un alert col nome dell'autore ogni volta che il cursore passa sopra l'autore del post.
+
+const authors=document.querySelectorAll(".blog-post-meta a")
+
+
+authors.forEach(author =>{
+author.addEventListener("mouseover",()=>{
+        alert(author.textContent)
+        
+    })
+})
+    
+
+    
+    /**/
