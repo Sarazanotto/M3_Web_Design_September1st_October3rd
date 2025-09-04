06e00fd63d653668d526ff5b6c625dd4bbb7d72f
sara zanotto <saraazanotto@gmail.com>
2025-09-04 17:46:22 +0200

    esercizio completo

--- W1/W1D1/EXERCISE_HTML_CSS/main.js
+++ W1/W1D1/EXERCISE_HTML_CSS/main.js
@@ -1,19 +1,22 @@
 // EXTRA Crea con JavaScript la funzionalità per rimuovere il link "twitter" sotto alla sezione "Altro" nell'elemento "aside". 
 // Deve avvenire al caricamento della pagina, automticamente.
 
-const linkTwitter = document.querySelector(".blog-sidebar .p-4:nth-of-type(3) ol li:nth-of-type(2)")
+/*const linkTwitter = document.querySelector(".blog-sidebar .p-4:nth-of-type(3) ol li:nth-of-type(2)")
 
 function linkTwitterRemouve() {
     if (linkTwitter)
         linkTwitter.remove()
 }
-linkTwitterRemouve()
+linkTwitterRemouve()*/
 
-// EXTRA Crea con JavaScript la funzionalità per rimuovere il corrispondente elemento padre dal DOM cliccando sul link "Continua a leggere".
 
 function removeElement(element) {
     element.remove();
 }
+
+removeElement(linkTwitter)
+
+// EXTRA Crea con JavaScript la funzionalità per rimuovere il corrispondente elemento padre dal DOM cliccando sul link "Continua a leggere".
 
 const anchorsToRemove = document.querySelectorAll(".col a, .jumbotron p>a");
 
@@ -23,16 +26,6 @@ anchorsToRemove.forEach(anchor => {
         removeElement(parent)
     })
 })
-
-
-/*removeElement(linkTwitter)*/
-
-
-/*function removeFatherElement(){
-    father.remove()
-}*/
-
-
 
 // EXTRA Crea con JavaScript la funzionalità per creare un alert col nome dell'autore ogni volta che il cursore passa sopra l'autore del post.
 
